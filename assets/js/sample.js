62c8f98f87350aee4d98b9142099492bd67af401
petersenrr <petersenrr@ldschurch.org>
2017-01-18 13:15:04 -0700

    changed the templates populated

--- assets/js/sample.js
+++ assets/js/sample.js
@@ -1,6 +1,11 @@
 (function() {
-  (document.getElementById("gen")).style.display = 'none';
-  (document.getElementById("advance")).style.display = 'none';
+  (document.getElementById("husbandsSection")).style.display = 'none';
+  (document.getElementById("wivesSection")).style.display = 'none';
+  (document.getElementById("parentsSection")).style.display = 'none';
+  (document.getElementById("qualitySection")).style.display = 'none';
+  (document.getElementById("researchSection")).style.display = 'none';
+
+
   FS.baseUrl = "https://integration.familysearch.org";
   
   //set default for the locale dropdown
@@ -21,32 +26,186 @@
 
   function getCook(cookiename) 
   {
-  // Get name followed by anything except a semicolon
-  var cookiestring=RegExp(""+cookiename+"[^;]+").exec(document.cookie);
-  // Return everything after the equal sign, or an empty string if the cookie name not found
-  return unescape(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+    // Get name followed by anything except a semicolon
+    var cookiestring=RegExp(""+cookiename+"[^;]+").exec(document.cookie);
+    // Return everything after the equal sign, or an empty string if the cookie name not found
+    return unescape(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
   }
-
-//Sample usage
-
-
   var cookieValue = getCook('fssessionid');
-
-
 })();
 
+
+
 var id;
+var templateSelected;
 
 function loadUrl(){
   window.open("https://integration.familysearch.org/tree/person/" + id,'_blank');;
 }
 
+function hideAll(){
+  (document.getElementById("husbandsSection")).style.display = 'none';
+  (document.getElementById("wivesSection")).style.display = 'none';
+  (document.getElementById("parentsSection")).style.display = 'none';
+  (document.getElementById("qualitySection")).style.display = 'none';
+  (document.getElementById("researchSection")).style.display = 'none';
+  (document.getElementById("childrenSection")).style.display = 'none';
+  (document.getElementById("marriageSection")).style.display = 'none';
+  (document.getElementById("templeSection")).style.display = 'none';
+  (document.getElementById("gen")).style.display = 'none';
+  (document.getElementById("dateSection")).style.display = 'none';
+  (document.getElementById("placeSection")).style.display = 'none';
+  (document.getElementById("livingSection")).style.display = 'none';
+  (document.getElementById("privateSection")).style.display = 'none';
+}
+
 
 function listQ(){
  var e = document.getElementById("dropTemplate");
- if(e.selectedIndex > 0){
-  if("Generational" === e.options[e.selectedIndex].value){ 
-    (document.getElementById("gen")).style.display = 'block';
+ if(e.selectedIndex > -1){
+  templateSelected = e.options[e.selectedIndex].value;
+  hideAll();
+  switch(templateSelected){
+    case "Basic":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      (document.getElementById("livingSection")).style.display = 'block';
+      (document.getElementById("privateSection")).style.display = 'block';
+      break;
+    }
+    case "Indicator.DataQuality":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    case "Default":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      (document.getElementById("livingSection")).style.display = 'block';
+      (document.getElementById("privateSection")).style.display = 'block';
+      break;
+    }
+    case "Extended":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      break;
+    }
+    case "Generational":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      (document.getElementById("livingSection")).style.display = 'block';
+      (document.getElementById("privateSection")).style.display = 'block';
+      break;
+    }
+    case "Indicators":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    case "Living":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      break;
+    }
+    case "Localized":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      (document.getElementById("livingSection")).style.display = 'block';
+      (document.getElementById("privateSection")).style.display = 'block';
+      break;
+    }
+    case "Multiple.Husbands":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("husbandsSection")).style.display = 'block';
+      break;
+    }
+    case "Multiple.Parents":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("parentsSection")).style.display = 'block';
+      break;
+    }
+    case "Multiple.Wives":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("wivessSection")).style.display = 'block';
+      break;
+    }
+    case "Private":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("gen")).style.display = 'block';
+      (document.getElementById("dateSection")).style.display = 'block';
+      (document.getElementById("placeSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      (document.getElementById("marriageSection")).style.display = 'block';
+      (document.getElementById("templeSection")).style.display = 'block';
+      (document.getElementById("livingSection")).style.display = 'block';
+      break;
+    }
+    case "publicPrivate":{
+      break;
+    }
+    case "Indicator.ResearchSuggestion":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    case "Single.Father":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    case "Single.Mother":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    case "Temple":{
+      (document.getElementById("familyNameSection")).style.display = 'block';
+      (document.getElementById("childrenSection")).style.display = 'block';
+      (document.getElementById("localeSection")).style.display = 'block';
+      break;
+    }
+    default:
+      break;
+
   }
  }
 }
@@ -66,7 +225,6 @@ function createFamily() {
     var child = document.getElementById("children").value;
     var temple = document.getElementById("templeReady").value;
     var marriage = document.getElementById("drop").value;
-    var template= document.getElementById("dropTemplate").value;
     var generations = document.getElementById("generations").value;
 
     var date = document.getElementById("date").value;
@@ -91,42 +249,103 @@ function createFamily() {
             var jsonResponse = JSON.parse(data);
             name = jsonResponse.persons[0].names[0].nameForms[0].fullText;
 
-            id = jsonResponse.persons[0].id;
-            document.cookie = "createdId="+escape(id);  
-            document.cookie = "createdName="+escape(name);       
+            id = jsonResponse.persons[0].id;    
             
-            console.log(jsonResponse);
             loadUrl();
-            setTimeout(3000);
-            
+
       }
     };
 
-
-    if((document.getElementById("advance")).style.display === 'block')
-    {
-      if((document.getElementById("gen")).style.display === 'block')
-      {
-        xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + template + "&children=" + child + "&marriage=" + marriage + "&generations=" + generations +
-          "&temple=" + temple + "&date=" + date + "&familyName=" + familyName + "&husbands=" + husbands + "&wives=" + wives + "&locale=" + locale +
-          "&parents=" + parents + "&place=" + place + "&living=" + living + "&dataQuality=" + dataQuality + "&researchSuggestion=" + research + "&private=" + private);
-      }
-      else {
-        xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&children=" + child + "&marriage=" + marriage + "&temple=" + temple + "&date=" + date + 
-          "&familyName=" + familyName + "&husbands=" + husbands + "&wives=" + wives + "&locale=" + locale +
-          "&parents=" + parents + "&place=" + place + "&living=" + living + "&dataQuality=" + dataQuality + "&researchSuggestion=" + research + "&private=" + private);
-      }
+    switch(templateSelected){
+      case "Basic":{
+        xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + living + "&private=" + private);
+      break;
     }
-    else
-    {
-      if((document.getElementById("gen")).style.display === 'block')
-      {
-        xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + template + "&children=" + child + "&marriage=" + marriage + 
-          "&generations=" + generations + "&temple=" + temple);
-      }
-      else {
-        xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&children=" + child + "&marriage=" + marriage + "&temple=" + temple);
-      }
+    case "Indicator.DataQuality":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&locale=" + locale);
+      break;
+    }
+    case "Default":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + living + "&private=" + private);
+      break;
+    }
+    case "Extended":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&children=" + child + 
+          "&locale=" + locale);
+      break;
+    }
+    case "Generational":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + living + "&private=" + private);
+      break;
+    }
+    case "Indicators":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&locale=" + locale);
+      break;
+    }
+    case "Living":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + true + "&private=" + true);
+      break;
+    }
+    case "Localized":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + living + "&private=" + private);
+      break;
+    }
+    case "Multiple.Husbands":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&husbands=" + husbands + 
+        "&children=" + child + "&locale=" + locale);
+      break;
+    }
+    case "Multiple.Parents":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&parents=" + parents + 
+        "&children=" + child + "&locale=" + locale);
+      break;
+    }
+    case "Multiple.Wives":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&wives=" + wives + 
+        "&children=" + child + "&locale=" + locale);
+      break;
+    }
+    case "Private": {
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName +"&children=" + child + 
+          "&generations=" + generations + "&date=" + date + "&place=" + place + "&locale=" + locale + "&marriage=" + marriage + "&temple=" + temple +
+          "&living=" + living + "&private=" + true);
+      break;
+    }
+    case "publicPrivate":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected);
+      break;
+    }
+    case "Indicator.ResearchSuggestion":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&locale=" + locale);
+      break;
+    }
+    case "Single.Father":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&children=" + child + 
+          "&locale=" + locale);
+      break;
+    }
+    case "Single.Mother":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&children=" + child + 
+          "&locale=" + locale);
+      break;
+    }
+    case "Temple":{
+      xhr.open("POST", "/test-data/data/family?sessionId=" + session + "&template=" + templateSelected + "&familyName="+ familyName + "&children=" + child + 
+          "&locale=" + locale);
+      break;
+    }
+    default:
+      break;
     }
     
     xhr.setRequestHeader('Content-Type', 'application/json');
